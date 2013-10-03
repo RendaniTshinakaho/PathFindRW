4be0ddbb4ebfaf15954a896fd8de54244fc075d8
RendaniTshinakaho <r.nkhumeleni@gmail.com>
2013-10-03 16:36:14 +0200

    First Commit

---
+++ PathFinder.App/BoardChart/Board.cs
@@ -0,0 +1,13 @@
+﻿namespace BoardChart
+{
+    public class Board
+    {
+        int[] currentState = new int[]{};
+        int[] goalState = new int[]{1,2,3,4,0,5,6,7,8};
+        int[] openQueue = new int[]{};
+        int[] closedState = new int[]{};
+
+        public 
+    }
+
+}
---
+++ PathFinder.App/GameTest/Test.cs
@@ -0,0 +1,16 @@
+﻿using BoardChart;
+using NUnit.Framework;
+
+namespace GameTest
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void CanInstantiateBoard()
+        {
+            Assert.That(new Board(),Is.Not.Null);
+        }
+
+    }
+}
